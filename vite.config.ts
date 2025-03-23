db53107f68a7d05e20d3a8aee8fdb6fa3a7d9b6d
acsobony <acsobony@hotmail.com>
2025-03-23 23:15:48 +1100

    Update Vite configuration and fix preview server

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   build: {
     target: 'esnext',
     sourcemap: true
-  }
+  },
+  assetsInclude: ['**/*.gltf', '**/*.glb'] // Include any 3D model formats you might use
 });
